541aed546b2341dcd91e246a57f3a3fc7b48c2bc
naderkamelaponar <nader.kamel.the.programmer@gmail.com>
2023-06-01 19:57:09 +0300

    init login/logout routes

--- server.js
+++ server.js
@@ -56,13 +56,22 @@ function ensureAuthenticated(req,res,next){
 app.route("/login").post(passport.authenticate('local',{failureRedirect:"/"}),(res,req)=>{
   res.redirect('/profile')
 });
+app.route('/logout',(req,res)=>{
+  req.logout()
+  res.redirect("/");
+})
 app.route("/profile").get(ensureAuthenticated,(req,res)=>{
-  res.render("profile")
+  res.render("profile",{username:req.user.username})
 })
 });
 app.route('/').get((req, res) => {
   res.render('index', { title: 'Connected to Database', message: 'Please log in' ,showLogin:true});
 
+});
+app.use((req, res, next) => {
+  res.status(404)
+    .type('text')
+    .send('Not Found');
 });
 
 /** change above here */
