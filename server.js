56c257304b67a394939d57c93df759e8eb92e296
naderkamelaponar <nader.kamel.the.programmer@gmail.com>
2023-06-01 23:10:17 +0300

    init register route

--- server.js
+++ server.js
@@ -46,6 +46,7 @@ myDB(async (client) => {
       title: "Connected to Database",
       message: "Please log in",
       showLogin: true,
+      showRegistration:true,
     });
   });
   const myDataBase = await client.db("database").collection("users");
@@ -73,6 +74,22 @@ myDB(async (client) => {
   app.route("/profile").get(ensureAuthenticated, (req, res) => {
     res.render("profile", { username: req.user.username });
   });
+  app.route("/register").post((req,res,next)=>{
+    myDataBase.findOne({username:req.body.username},(err,user)=>{
+      if(err){next(err)}
+      else if (user){res.redirect("/")}
+      else{
+        /** insert new */
+        myDataBase.insertOne({
+          username:req.body.username,
+          password:req.body.password
+        },(err,doc)=>{
+          if(err){res.redirect("/")}
+          else{next(null,doc.ops[0])}
+        })
+      }
+    })
+  })
 
   app.use((req, res, next) => {
     res.status(404).type("text").send("Not Found");
