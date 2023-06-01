b5f72693fbfc06756cafef9ae6494813246a6b8f
naderkamelaponar <nader.kamel.the.programmer@gmail.com>
2023-06-01 14:56:33 +0300

    init local strategy

--- server.js
+++ server.js
@@ -9,13 +9,25 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+/** start coding point */
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
 const passport=require('passport');
-/** change below here */
+const LocalStrategy=require('passport-local');
 const { ObjectID } = require('mongodb');
 const session =require('express-session');
 
+/** change below here */
+const passLocal= new LocalStrategy((username,password,done)=>{
+  myDataBase.findOne({username:username},(err,user)=>{
+    console.log(`User ${username} attempted to log in.`)
+    if (err)return done(err);
+    if (!user) return done(null, false);
+    if (password!==user.password) return done(null,false);
+    return done(null,user);
+  })
+})
+passport.use(passLocal);
 app.use(session({
   secret:process.env.SESSION_SECRET,
   resave:true,
@@ -32,7 +44,7 @@ passport.serializeUser((user,done)=>{
 passport.deserializeUser((id,done)=>{
   myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
     done(null, doc);
-  });
+  }); 
 })
 })
 
