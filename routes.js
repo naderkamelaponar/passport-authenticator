4949359241d6a018bb10d63b321daedb15efd09d
naderkamelaponar <nader.kamel.the.programmer@gmail.com>
2023-06-15 17:50:42 +0300

    fix Handle a Disconnect

--- routes.js
+++ routes.js
@@ -24,10 +24,6 @@ module.exports = function (app, myDataBase) {
       //res.json({user:req.user});
     }
   );
-  app.route("/logout").get((req, res) => {
-    req.logout();
-    res.redirect("/");
-  });
   app.route("/profile").get(ensureAuthenticated, (req, res) => {
     console.log(req)
     res.render("profile", { username: req.user.name });
@@ -75,7 +71,10 @@ module.exports = function (app, myDataBase) {
       //res.json({user:req.user});
     }
   );
-
+  app.route("/logout").get((req, res) => {
+    req.logout();
+    res.redirect("/");
+  });
   app.use((req, res, next) => {
     res.status(404).type("text").send("Not Found");
   });
